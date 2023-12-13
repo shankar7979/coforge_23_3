af9a788473c91b19f0f28164418d817894fe80dc
shankar7979 <shankar7979@gmail.com>
2023-12-13 15:57:31 +0530

    first commit

--- corejava/src/com/coforge/exception/ExceptionProgram1.java
+++ corejava/src/com/coforge/exception/ExceptionProgram1.java
@@ -2,23 +2,43 @@ package com.coforge.exception;
 
 public class ExceptionProgram1 {
 
-	
 	public static void main(String[] args) {
-		
 		System.out.println("program starts ");
-		
-		int ar[]= {1,2,3,4,5};
+
+		int ar[] = { 1, 2, 3, 4, 5 };
+		String name = "ram kumar";
+		String s = null;
+
 		try {
-		System.out.println("fifth element is "+ar[5]);
-		//ArrayIndexOutOfBoundsException
+			System.out.println(s.length());
+			System.out.println("fifth element is " + ar[5]);
+			// ArrayIndexOutOfBoundsException
+
+			System.out.println(name);
+			System.out.println(name.charAt(9));
+			// StringIndexOutOfBoundsException
 		}
+
+		catch (NullPointerException | StringIndexOutOfBoundsException | ArrayIndexOutOfBoundsException e) {
+			System.err.println(e.getMessage());
+		}
+
+//		catch (NullPointerException e) {
+//			 System.err.println(e.getMessage());
+//		}
+//		catch (StringIndexOutOfBoundsException e) {
+//			 System.err.println(e.getMessage());
+//		}
+//		catch (ArrayIndexOutOfBoundsException e) {
+//			 System.err.println(e.getMessage());
+//		}
+
 		catch (Exception e) {
 			System.out.println("some error");
+			e.printStackTrace();
 		}
 		System.out.println("program ends ");
-		
-		
+
 	}
-	
-	
+
 }
