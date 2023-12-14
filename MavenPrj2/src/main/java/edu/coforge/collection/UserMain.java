2468ba450bebef3da4c6604ce153999cef7d1753
shankar7979 <shankar7979@gmail.com>
2023-12-14 14:56:19 +0530

    first commit

--- MavenPrj2/src/main/java/edu/coforge/collection/UserMain.java
+++ MavenPrj2/src/main/java/edu/coforge/collection/UserMain.java
@@ -5,10 +5,16 @@ public class UserMain {
 	public static void main(String[] args) {
 		
 		
-		User user=new User(56765567, "amarnath gupta");
+		User user1=new User(56765567, "amarnath gupta");
+		User user2=new User(56765567, "amarnath gupta");
 		
-		System.out.println(user);
-		System.out.println(user.toString());
+		System.out.println(user1);
+		System.out.println(user2);
+		System.out.println(user1.equals(user2));
+		
+		System.out.println(user1.hashCode());
+		System.out.println(user2.hashCode());
+		
 		
 		 
 	}
