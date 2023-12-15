1bd9ab6e78bfdc27375611a7ca88159f5be19600
shankar7979 <shankar7979@gmail.com>
2023-12-15 12:34:08 +0530

    first commit

--- MavenPrj2/src/main/java/edu/coforge/java8/java8TimePrg1.java
+++ MavenPrj2/src/main/java/edu/coforge/java8/java8TimePrg1.java
@@ -12,8 +12,14 @@ public class java8TimePrg1 {
 		System.out.println(localTime);
 		System.out.println(LocalTime.NOON);
 
-		LocalTime time1 = LocalTime.of(10, 20, 30);
+		LocalTime time1 = LocalTime.of(10, 20, 30,45);
 		System.out.println(time1);
+		
+		System.out.println("hh:mm:ss format"+DateTimeFormatter.ofPattern("hh:mm:ss").format(time1));
+		
+		System.out.println("hh:mm format "+DateTimeFormatter.ofPattern("hh:mm").format(time1));
+		
+		System.out.println("hh:mm:ss n a format"+DateTimeFormatter.ofPattern("hh:mm:ss:n a").format(time1));		
 
 		LocalTime time2 = time1.plusHours(10);
 		System.out.println(time2);
