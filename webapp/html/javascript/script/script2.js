c9d36f340ece9862154e445e6253531ea05ba6b8
shankar7979 <shankar7979@gmail.com>
2023-12-18 09:39:47 +0530

    first commit

--- webapp/html/javascript/script/script2.js
+++ webapp/html/javascript/script/script2.js
@@ -4,3 +4,11 @@ function addition(a, b) {
 function subtraction(a, b) {
     alert("subtraction is " + (a - b));
 }
+
+function factorial(k){
+    var fact=1;
+    for (var x=1;x<=k;x++) {
+         fact=fact*x;
+    }
+    return fact;
+}
