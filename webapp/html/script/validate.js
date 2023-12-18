f9359f537f76e96669a23466acfa56b281143865
shankar7979 <shankar7979@gmail.com>
2023-12-18 14:20:38 +0530

    first commit

--- webapp/html/script/validate.js
+++ webapp/html/script/validate.js
@@ -25,11 +25,28 @@ function employee_validate() {
          document.getElementById('name').style.border = "2px solid red"
         return false
     }
-
     else if (age == '') {
         alert('age is blank')
         document.getElementById('name').style.border = "1px solid black"
         document.getElementById('age').style.border = "2px solid red";
+        return false
+    }
+    else if (time == '') {
+        alert('login time is blank')
+        document.getElementById('time').style.border = "2px solid red"
+        document.getElementById('age').style.border = "1px solid black";
+        return false
+    }
+    
+    else if (email == '') {
+        alert('email is blank')
+        document.getElementById('time').style.border = "1px solid black"
+        document.getElementById('email').style.border = "2px solid red";
+        return false
+    }
+    else if (!email.endsWith(".com")) {
+        alert('invalid email\nmust ends with .com')
+        document.getElementById('email').style.border = "2px solid red";
         return false
     }
 
