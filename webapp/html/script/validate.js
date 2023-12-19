05ae26368aa946e36667c4f7ce498b4481c68edb
shankar7979 <shankar7979@gmail.com>
2023-12-19 09:35:29 +0530

    first commit

--- webapp/html/script/validate.js
+++ webapp/html/script/validate.js
@@ -22,48 +22,55 @@ function employee_validate() {
         //alert('id is blank')
         document.getElementById('id_err').innerHTML='id is blank'
         document.getElementById('id').style.border = "2px solid red"
-
         return false
     }
 
     else if (name == '') {
-        alert('name is blank')
+        //alert('name is blank')
+        document.getElementById('id_err').innerHTML=''
+        document.getElementById('name_err').innerHTML='name is blank'
         document.getElementById('id').style.border = "1px solid black";
         document.getElementById('name').style.border = "2px solid red"
         return false
     }
     else if (name.length<5 || name.length>10) {
-        alert('name length must of 5 - 10 chars')
+        document.getElementById('name_err').innerHTML='name length must of 5 - 10 chars'
+        //alert('name length must of 5 - 10 chars')
          document.getElementById('name').style.border = "2px solid red"
         return false
     }
     else if (age == '') {
-        alert('age is blank')
+        //alert('age is blank')
+        document.getElementById('age_err').innerHTML='age is blank'
         document.getElementById('name').style.border = "1px solid black"
         document.getElementById('age').style.border = "2px solid red";
         return false
     }
     else if (time == '') {
-        alert('login time is blank')
+        //alert('login time is blank')
         document.getElementById('time').style.border = "2px solid red"
         document.getElementById('age').style.border = "1px solid black";
+        document.getElementById('time_err').innerHTML='login time is blank'
         return false
     }
     
     else if (email == '') {
-        alert('email is blank')
+        //alert('email is blank')
         document.getElementById('time').style.border = "1px solid black"
         document.getElementById('email').style.border = "2px solid red";
+        document.getElementById('email_err').innerHTML='email is blank'
         return false
     }
     else if (!email.endsWith(".com")) {
-        alert('invalid email\nmust ends with .com')
+        //alert('invalid email\nmust ends with .com')
+        document.getElementById('email_err').innerHTML='invalid email must ends with .com'
         document.getElementById('email').style.border = "2px solid red";
         return false
     }
     
     else if (gstatus==false) {
-        alert('select the gender')
+        //alert('select the gender')
+        document.getElementById('gender_err').innerHTML='select the gender'
         document.getElementById('email').style.border = "1px solid black";
         document.getElementById('gender1').style.border = "2px solid red";
         return false
