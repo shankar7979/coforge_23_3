843f41e94385d5876119fecc43163d1e5bec5562
shankar7979 <shankar7979@gmail.com>
2023-12-21 11:31:17 +0530

    first commit

--- ss.bat
+++ ss.bat
@@ -1,4 +1,4 @@
-cp -r C:\Users\Training\eclipse-workspace1\WebApp1 .
+cp -r C:\Users\Training\eclipse-workspace1\ShoppingKart .
  git add *
   git commit -m "first commit"
   git push -u origin main
