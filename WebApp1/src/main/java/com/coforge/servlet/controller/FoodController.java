4cf11412f0d99c88bcc5197a92c22196723437ff
shankar7979 <shankar7979@gmail.com>
2023-12-21 09:39:16 +0530

    first commit

--- WebApp1/src/main/java/com/coforge/servlet/controller/FoodController.java
+++ WebApp1/src/main/java/com/coforge/servlet/controller/FoodController.java
@@ -10,34 +10,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
- 
 @WebServlet("/FoodController")
 public class FoodController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-     
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		PrintWriter writer = response.getWriter();
-	
 		String type = request.getParameter("type");
-		
+
 		RequestDispatcher dispatcher;
-		
-		if(type.equals("nindian")){
-			
-	dispatcher=request.getRequestDispatcher("/view/north_food.jsp");
-	dispatcher.forward(request, response);		
-			
+		if (type.equals("nindian")) {
+			dispatcher = request.getRequestDispatcher("/view/north_food.jsp");
+			dispatcher.forward(request, response);
 		}
-		
-		if(type.equals("sindian")){
-			
-			dispatcher=request.getRequestDispatcher("/view/south_food.jsp");
-			dispatcher.forward(request, response);		
-			
+
+		if (type.equals("sindian")) {
+			dispatcher = request.getRequestDispatcher("/view/south_food.jsp");
+			dispatcher.forward(request, response);
 		}
-		
+
 	}
- 
 
 }
