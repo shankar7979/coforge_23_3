d78af3d036e87008641b7e7542bdd18ee49ac0d9
shankar7979 <shankar7979@gmail.com>
2023-12-22 15:36:36 +0530

    first commit

---
+++ node_app1/database1.js
@@ -0,0 +1,18 @@
+var mysql=require('mysql')
+var connection=mysql.createConnection({
+     'host':'localhost',
+     'user':'root',
+     'password':'mysql',
+     database:'db1'
+
+});
+connection.connect((err)=>{
+    if(err)
+      throw err;
+    connection.query("select * from employee",(err,result)=>{
+        if(err)
+         throw err;
+        console.log(result);
+    });
+console.log('connected ')
+})
---
+++ node_app1/readme.txt
@@ -0,0 +1,7 @@
+npm init 
+
+npm install mysql
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'mysql';
+flush privileges;
+
