ad16979ca7c3523d47155e424bcf01327d1d3e11
shankar7979 <shankar7979@gmail.com>
2023-12-22 16:48:22 +0530

    first commit

--- node_app1/nodeserver1.js
+++ node_app1/nodeserver1.js
@@ -1,14 +1,15 @@
 var http=require('http');
 
 http.createServer((req,resp)=>{
-    resp.writeHead(200,{'ContentType':'text/html'})
+    resp.writeHead(200,{'Content-Type':'text/html'})
     resp.write('<br>hello world');
     ar=["delhi","chennai","kolkotta","mumbai","banglore","pune"]
     resp.write('<ol>')
     ar.forEach(element => {
         resp.write("<li>"+element)
     });
-    
+    //setInterval(()=>console.log(new Date().toTimeString()),1000)
+    setInterval(()=>resp.write(new Date().toTimeString()),1000)
     resp.write('</ol>')
     resp.write('my clock')
     
