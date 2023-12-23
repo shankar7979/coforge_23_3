0e0526a7c494db6b532b40dfbd893f652b05c3cb
shankar7979 <shankar7979@gmail.com>
2023-12-23 11:12:34 +0530

    first commit

--- node_app1/employee_app1.js
+++ node_app1/employee_app1.js
@@ -1,4 +1,4 @@
-var employee=require('./data/employee.js');
+var employee=require('./data/employee');
 var express=require('express')
 var app=express();
 app.use(express.json())
@@ -12,6 +12,22 @@ app.get("/",(req,resp)=>{
 app.get("/all",(req,resp)=>{
    console.log('all employee get method ')
    resp.json(employee);
+});
+
+app.get("/all/:eid",(req,resp)=>{
+   console.log(' employee search  method  using id ')
+   if(!req.params.eid ||isNaN(req.params.eid) ){
+   return resp.status(404).send('invalid id  or not a number')
+   }
+
+   var e=employee.find((a)=>{
+    return a.id===parseInt(req.params.eid)
+   })
+ if(!e){
+    return resp.status(404).send('id is not found')
+ }
+
+  return  resp.status(201).send(e);
 });
 
 
