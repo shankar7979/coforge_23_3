0e730501c2d9c51ff6031e9c9e7e74cc3d35fd9b
shankar7979 <shankar7979@gmail.com>
2023-12-23 11:57:52 +0530

    first commit

--- node_app1/employee_app1.js
+++ node_app1/employee_app1.js
@@ -1,34 +1,52 @@
-var employee=require('./data/employee');
-var express=require('express')
-var app=express();
+var employee = require('./data/employee');
+var express = require('express')
+var app = express();
 app.use(express.json())
 
-app.get("/",(req,resp)=>{
-   resp.write('<h1>Employee Program</h1>')
-   console.log('get method with  root (/) path  ')
-   resp.end();
+app.get("/", (req, resp) => {
+    resp.write('<h1>Employee Program</h1>')
+    console.log('get method with  root (/) path  ')
+    resp.end();
 });
-
-app.get("/all",(req,resp)=>{
-   console.log('all employee get method ')
-   resp.json(employee);
+// get all operation
+app.get("/all", (req, resp) => {
+    console.log('all employee get method ')
+    resp.json(employee);
 });
+// search operation
+app.get("/all/:eid", (req, resp) => {
+    console.log(' employee search  method  using id ')
+    if (!req.params.eid || isNaN(req.params.eid)) {
+        return resp.status(404).send('invalid id  or not a number')
+    }
 
-app.get("/all/:eid",(req,resp)=>{
-   console.log(' employee search  method  using id ')
-   if(!req.params.eid ||isNaN(req.params.eid) ){
-   return resp.status(404).send('invalid id  or not a number')
-   }
+    var e = employee.find((a) => {
+        return a.id === parseInt(req.params.eid)
+    })
+    if (!e) {
+        return resp.status(404).send('id is not found')
+    }
 
-   var e=employee.find((a)=>{
-    return a.id===parseInt(req.params.eid)
-   })
- if(!e){
-    return resp.status(404).send('id is not found')
- }
-
-  return  resp.status(201).send(e);
+    return resp.status(302).send(e);
 });
-
+app.delete("/all/:eid", (req, resp) => {
+    console.log('delete operation using id ')
+    if (!req.params.eid || isNaN(req.params.eid)) {
+        return resp.status(404).send('invalid id <br>id is not a number');
+    }
+    var e = employee.find((a) => {
+        return a.id === parseInt(req.params.eid)
+    })
+    if (!e) {
+        return resp.status(404).send('id is not found')
+        console.log('id not found ')
+    }
+  if(e){
+    let  index= employee.indexOf(e);
+    employee.splice(index,1);
+    console.log('found and deleted')
+    return resp.status(202).json(employee);
+  }
+})
 
 app.listen(1000);
