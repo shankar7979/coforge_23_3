311e98bbe426bb107b195cf31fed636499850d16
shankar7979 <shankar7979@gmail.com>
2023-12-26 09:52:13 +0530

    first commit

---
+++ HibernateProgram1/src/main/java/com/coforge/model/Customer.java
@@ -0,0 +1,38 @@
+package com.coforge.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+	@Id
+	private int customerId;
+	private String customerName;
+	private float customerSalary;
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public float getCustomerSalary() {
+		return customerSalary;
+	}
+
+	public void setCustomerSalary(float customerSalary) {
+		this.customerSalary = customerSalary;
+	}
+
+}
