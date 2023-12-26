a1903056244aa3760410f635f10a760adb842177
shankar7979 <shankar7979@gmail.com>
2023-12-26 15:26:54 +0530

    first commit

--- ss.bat
+++ ss.bat
@@ -1,5 +1,7 @@
 
-cp -r C:\Users\Training\eclipse-workspace1\HibernateProgram1 .
+cp -r C:\Users\Training\eclipse-workspace1\Test_project .
+rem cp -r C:\Users\Training\eclipse-workspace1\HibernateProgram1 .
+
  git add *
   git commit -m "first commit"
   git push -u origin main
