8a1096b340acde2d068f4a469dd9a5efbf83b6c7
shankar7979 <shankar7979@gmail.com>
2023-12-26 10:39:31 +0530

    first commit

--- HibernateProgram1/src/main/java/com/coforge/main/CustomerMain.java
+++ HibernateProgram1/src/main/java/com/coforge/main/CustomerMain.java
@@ -1,9 +1,13 @@
 package com.coforge.main;
 
+import java.util.List;
+import java.util.Scanner;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import com.coforge.model.Customer;
 
@@ -11,25 +15,49 @@ public class CustomerMain {
 
 	public static void main(String[] args) {
 
-		Configuration configuration=new Configuration();
+		Configuration configuration = new Configuration();
 		configuration.configure();
-		
-		SessionFactory sessionFactory = configuration.buildSessionFactory();
-		
-		Session session = sessionFactory.openSession();
-		
-		Transaction transaction = session.getTransaction();
-		
-		Customer customer=new Customer();
 
-		customer.setCustomerId(676776767);
-		customer.setCustomerName("suresh kumar");
-		customer.setCustomerSalary(35000.00f);
-		
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+		Session session = sessionFactory.openSession();
+
+		Transaction transaction = session.getTransaction();
+
+		Customer customer = new Customer();
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("enter id name and salary ");
+
+		customer.setCustomerId(scanner.nextInt());
+		scanner.nextLine();
+		customer.setCustomerName(scanner.nextLine());
+		customer.setCustomerSalary(scanner.nextFloat());
+
+//		customer.setCustomerId(676776767);
+//		customer.setCustomerName("suresh kumar");
+//		customer.setCustomerSalary(35000.00f);
 
 		transaction.begin();
 		session.save(customer);
-		transaction.commit();
+
+		System.out.println("save data into database");
+		if (scanner.next().equals("yes"))
+			transaction.commit();
+		else
+			transaction.rollback();
+
+		System.out.println("all employee records ");
+	   
+		// hql -- hibernate query language 
 		
+		 Query<Customer> createQuery = session.createQuery("from Customer");
+	     List<Customer> resultList = createQuery.getResultList();
+	     
+	     for (Customer customer2 : resultList) {
+			System.out.println(customer2.getCustomerId()+"\t"+customer2.getCustomerName()+"\t"+customer2.getCustomerSalary());
+		}
+	     
 	}
 }
