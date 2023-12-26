df87d62a34578cf51073ed2add27c01e8536f02e
shankar7979 <shankar7979@gmail.com>
2023-12-26 14:25:21 +0530

    first commit

--- HibernateProgram1/src/main/java/com/coforge/main/CustomerMain2.java
+++ HibernateProgram1/src/main/java/com/coforge/main/CustomerMain2.java
@@ -25,6 +25,8 @@ public class CustomerMain2 {
 			System.out.println("1.Add Customer");
 			System.out.println("2.Search Customer");
 			System.out.println("3.List Of All Customer");
+			System.out.println("4.Delete Customer");
+			System.out.println("5.Update Customer");
 			int key = scanner.nextInt();
 
 			switch (key) {
@@ -63,8 +65,44 @@ public class CustomerMain2 {
 				else
 				dao.getAllCustomer().forEach(a->System.out.println(a));
 			break;
+
+			case 4:
+				System.out.println("enter customer id");
+				customer=null;
+				customer=dao.deleteCustomer(scanner.nextInt());
+				if(customer==null)
+					System.out.println("Record not found with id ");
+				else 
+					System.out.println("Customer Deleted\n"+customer);
+				break;
+				
+
+			case 5:
+				customer = null;
+				customer = new Customer();
+				System.out.println("Enter id name salary and dob in dd-MM-yyyy format ");
+				
+				customer.setCustomerId(scanner.nextInt());
+				
+				scanner.nextLine();
+				customer.setCustomerName(scanner.nextLine());
+				customer.setCustomerSalary(scanner.nextFloat());
+				dob = scanner.next();
+				 dob1=null;
+				dob1 = LocalDate.parse(dob, dateTimeFormatter);
+				customer.setCustomerDob(dob1);
+
+				
+				Customer updateCustomer = dao.updateCustomer(customer);
+				if(updateCustomer!=null) {
+					System.out.println("customer update");
+				}
+				else 
+					System.out.println("customer not present");
+				break;
 			
 			default:
+				System.out.println("Invalid Operation");
 				break;
 			}
 			System.out.println("continue y\\n");
