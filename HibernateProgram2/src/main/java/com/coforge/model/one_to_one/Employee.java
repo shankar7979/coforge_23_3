7117468d70cf51b80ffbf2abba0462b1828072df
shankar7979 <shankar7979@gmail.com>
2023-12-27 16:39:29 +0530

    first commit

--- HibernateProgram2/src/main/java/com/coforge/model/one_to_one/Employee.java
+++ HibernateProgram2/src/main/java/com/coforge/model/one_to_one/Employee.java
@@ -10,7 +10,12 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Getter
+@Setter
 public class Employee {
 
 	@Id
@@ -25,40 +30,4 @@ public class Employee {
 	
 	@OneToMany(cascade = CascadeType.ALL)
 	private Set<Car> carSet;
-	
-	public Set<Car> getCarSet() {
-		return carSet;
-	}
-	public void setCarSet(Set<Car> carSet) {
-		this.carSet = carSet;
-	}
-	public Address getAddress() {
-		return address;
-	}
-	public void setAddress(Address address) {
-		this.address = address;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public float getSalary() {
-		return salary;
-	}
-	public void setSalary(float salary) {
-		this.salary = salary;
-	}
-	
-	
-	
-	
-	
 }
