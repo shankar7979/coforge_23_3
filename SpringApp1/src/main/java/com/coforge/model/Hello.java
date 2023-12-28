fd4a422ffd4d377da1fb0dda8552a05a20048bea
shankar7979 <shankar7979@gmail.com>
2023-12-28 12:02:36 +0530

    first commit

--- SpringApp1/src/main/java/com/coforge/model/Hello.java
+++ SpringApp1/src/main/java/com/coforge/model/Hello.java
@@ -1,8 +1,19 @@
 package com.coforge.model;
 
 public class Hello {
-
+    int c;
+	void start() {
+		c=1;
+		System.out.println("init called ");
+	}
+	
 	public void display() {
 		System.out.println("display  method ");
+		System.out.println("no is  "+c);
 	}
+	
+	void destroy() {
+		System.out.println("destroy called ");
+	}
+	
 }
