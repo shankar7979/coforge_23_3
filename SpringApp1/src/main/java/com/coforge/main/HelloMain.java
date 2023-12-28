62153be5b159278da2bb5f1247f91bdbbeb0ec00
shankar7979 <shankar7979@gmail.com>
2023-12-28 12:07:34 +0530

    first commit

--- SpringApp1/src/main/java/com/coforge/main/HelloMain.java
+++ SpringApp1/src/main/java/com/coforge/main/HelloMain.java
@@ -1,6 +1,7 @@
 package com.coforge.main;
 
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.coforge.model.Hello;
@@ -9,10 +10,16 @@ public class HelloMain {
 
 	public static void main(String[] args) {
 
-		BeanFactory beanFactory = new ClassPathXmlApplicationContext("beans.xml");
+		//BeanFactory beanFactory 
+	    ConfigurableApplicationContext beanFactory	= new       
+	    		    ClassPathXmlApplicationContext("beans.xml");
 
 		Hello hello = (Hello) beanFactory.getBean("hello1");
 		hello.display();
+		
+		beanFactory.close();
+		
+		
 
 	}
 }
