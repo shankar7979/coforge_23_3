c663610ba94cf986d4cfe4b66b4db8629c46f590
shankar7979 <shankar7979@gmail.com>
2023-12-29 15:17:58 +0530

    first commit

--- ss.bat
+++ ss.bat
@@ -1,5 +1,5 @@
 
- cp -r C:\Users\Training\eclipse-workspace1\SpringApp3 .
+ cp -r C:\Users\Training\eclipse-workspace1\SpringMvcApp1 .
 rem cp -r C:\Users\Training\eclipse-workspace1\HibernateProgram2 .
 
 rem cp -r C:\Users\Training\eclipse-workspace1\Test_maven_project .
