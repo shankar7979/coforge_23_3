cc1d76e694db8c668b7b7dc21553598205ed1563
shankar7979 <shankar7979@gmail.com>
2024-01-02 10:26:41 +0530

    first commit

---
+++ SpringApp_jdbc/src/main/java/com/coforge/dao/EmployeeDaoImpl.java
@@ -0,0 +1,31 @@
+package com.coforge.dao;
+
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.coforge.model.Employee;
+
+import lombok.Data;
+
+@Data
+@Repository
+public class EmployeeDaoImpl implements EmployeeDao {
+
+	JdbcTemplate template;
+
+	@Override
+	public List<Employee> getAllEmployee() {
+		return null;
+	}
+
+	@Override
+	public Employee saveEmployee(Employee employee) {
+		int r = template.update("insert into employee values(" + employee.getId() + "," + employee.getName() + ","
+				+ employee.getSalary() + ")");
+		if (r == 1)
+			return employee;
+		return null;
+	}
+}
