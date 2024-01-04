0e2f4366815ed97ca7140ea90e21e96ac9a4ee21
shankar7979 <shankar7979@gmail.com>
2024-01-04 11:06:57 +0530

    first commit

---
+++ SpringBootRestApp1/src/main/java/com/example/controller/PersonController.java
@@ -0,0 +1,38 @@
+package com.example.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Person;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+	static List<Person> personList;
+	static Person person[]=new Person[5];
+	static long pid[]= {787887,56567,676678,67676,67789};
+	static  String  pname[]= {"anand kumar","suresh kumar","lal babu","kamal sagar","ramesh kumar"};
+	static  float  psalary[]= {34000,90000,45000,39000,25000};
+
+	static {
+		personList=new ArrayList<>();
+		for (int i = 0; i < person.length; i++) {
+			person[i]=new Person();
+			person[i].setId(pid[i]);
+			person[i].setName(pname[i]);
+			person[i].setSalary(psalary[i]);
+			personList.add(person[i]);
+		}
+	}
+	
+	
+	@GetMapping("/all")
+	public List<Person> getAllPerson() {
+		return personList;
+	}
+}
