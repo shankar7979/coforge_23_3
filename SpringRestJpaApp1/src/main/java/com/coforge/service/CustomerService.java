a9f11c1d7a50ec8dee4d9acd1f9d5d40de65bbcf
shankar7979 <shankar7979@gmail.com>
2024-01-04 16:35:35 +0530

    first commit

--- SpringRestJpaApp1/src/main/java/com/coforge/service/CustomerService.java
+++ SpringRestJpaApp1/src/main/java/com/coforge/service/CustomerService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.coforge.dao.CustomerDao;
+import com.coforge.exception.CustomerException;
 import com.coforge.model.Customer;
 
 @Service
@@ -14,34 +15,47 @@ public class CustomerService {
 
 	@Autowired
 	CustomerDao dao;
-	
+
 	public Customer addCustomer(Customer c) {
-    	return	dao.save(c);
+		Optional<Customer> findById = dao.findById(c.getCustomerId());
+		if (findById.isPresent())
+			throw new CustomerException("customer already present");
+		else
+			return dao.save(c);
 	}
-	
+
 	public Customer updateCustomer(Customer c) {
-		return	dao.save(c);
+		Optional<Customer> findById = dao.findById(c.getCustomerId());
+		if (!findById.isPresent())
+			throw new CustomerException("customer not present");
+		else
+			return dao.save(c);
 	}
+
 	public Customer removeCustomer(long id) {
-			Optional<Customer> findById = dao.findById(id);
-			
-	         if(findById.isPresent()) {
-	        	 Customer customer=findById.get();
-	        	 dao.delete(customer); 
-	          return customer;
-	         }
-	         return null;
-	}
-	
-	public Customer searchCustomer(long id ) {
 		Optional<Customer> findById = dao.findById(id);
-		if(findById.isPresent())
+		if (findById.isPresent()) {
+			Customer customer = findById.get();
+			dao.delete(customer);
+			return customer;
+		} else
+			throw new CustomerException("customer not present");
+
+	}
+
+	public Customer searchCustomer(long id) {
+		Optional<Customer> findById = dao.findById(id);
+		if (findById.isPresent())
 			return findById.get();
-		
-		return null;
+		else
+			throw new CustomerException("customer not present");
 	}
+
 	public List<Customer> getAllCustomer() {
-		 return dao.findAll();
+		if(dao.findAll().size()==0)
+			throw new CustomerException("customer list is empty");
+		else	
+		return dao.findAll();
 	}
-	
+
 }
