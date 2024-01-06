7e947837f88bfe098d5b059e4e02f0b2db79981b
shankar7979 <shankar7979@gmail.com>
2024-01-06 12:35:10 +0530

    first commit

--- ss.bat
+++ ss.bat
@@ -6,6 +6,8 @@ cp -r C:\Users\Training\eclipse-workspace1\SpringRestJpaApp1 .
   cp -r C:\Users\Training\Downloads\SpringBootRestApp1\SpringBootRestApp1 .
 
 
+cp -r C:\Users\Training\eclipse-workspace1\SpringRestJpaApp2_Profiles .
+
  git add *
   git commit -m "first commit"
   git push -u origin main
