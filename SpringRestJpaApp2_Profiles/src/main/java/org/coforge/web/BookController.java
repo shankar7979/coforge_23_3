e48b6215acad3fb532834e9764b14f48e2dde210
shankar7979 <shankar7979@gmail.com>
2024-01-08 09:37:58 +0530

    first commit

--- SpringRestJpaApp2_Profiles/src/main/java/org/coforge/web/BookController.java
+++ SpringRestJpaApp2_Profiles/src/main/java/org/coforge/web/BookController.java
@@ -7,6 +7,7 @@ import org.coforge.repository.BookDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,8 @@ public class BookController {
 	BookDao dao;
 	
 	@PostMapping("/add")
-	public Book addBook(Book b) {
+	public Book addBook(@RequestBody Book  b) {
+		System.out.println("add book is "+b);
 	  return dao.save(b);
 	}
 	
