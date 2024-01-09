9d514c420a44eb51f433e92e15161d9ab9048b85
shankar7979 <shankar7979@gmail.com>
2024-01-09 16:09:45 +0530

    first commit

--- Project1/src/app/employee/employee.component.ts
+++ Project1/src/app/employee/employee.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Address } from './address';
 
 @Component({
   selector: 'app-employee',
@@ -11,11 +12,19 @@ export class EmployeeComponent {
    id:number;
    name:string;
    salary:number;
+   address:Address[];
 
    constructor(){
     this.id=98989898;
     this.name='ram kumar';
     this.salary=56000;
+    this.address=[
+      {'addr_id':100001, 'addr_location':'alpha','addr_city':'gr noida'},
+      {'addr_id':100003, 'addr_location':'beta','addr_city':'gr noida'},
+      {'addr_id':100002, 'addr_location':'ganesh nagar','addr_city':'new delhi'},
+      {'addr_id':1000010, 'addr_location':'pandav nagar','addr_city':'new delhi'},
+      {'addr_id':100007, 'addr_location':'barsane','addr_city':'mathura'}
+    ]
    }
    
 
