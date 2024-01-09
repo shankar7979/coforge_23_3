8e146534f42c22b900b4c1398d2f03e02b28fe43
shankar7979 <shankar7979@gmail.com>
2024-01-09 13:04:01 +0530

    first commit

--- ss.bat
+++ ss.bat
@@ -1,13 +1,13 @@
 
-  cp -r C:\Users\Training\eclipse-workspace1\SpringEurekaServer .
-cp -r C:\Users\Training\eclipse-workspace1\SpringRestJpaApp1 .
-  cp -r C:\Users\Training\Downloads\SpringBootRestApp1\SpringBootRestApp1 .
+rem  cp -r C:\Users\Training\eclipse-workspace1\SpringEurekaServer .
+rem cp -r C:\Users\Training\eclipse-workspace1\SpringRestJpaApp1 .
+ rem  cp -r C:\Users\Training\Downloads\SpringBootRestApp1\SpringBootRestApp1 .
 
-cp -r C:\Users\Training\eclipse-workspace1\SpringRestJpaApp2_Profiles .
-cp -r C:\Users\Training\eclipse-workspace1\SpringConfigClient .
-cp -r C:\Users\Training\eclipse-workspace1\SpringConfigServer .
+rem cp -r C:\Users\Training\eclipse-workspace1\SpringRestJpaApp2_Profiles .
+rem cp -r C:\Users\Training\eclipse-workspace1\SpringConfigClient .
+rem cp -r C:\Users\Training\eclipse-workspace1\SpringConfigServer .
 
-cp -r C:\Users\Training\eclipse-workspace1\SpringRestJpaApp_RestTemplate .
+rem cp -r C:\Users\Training\eclipse-workspace1\SpringRestJpaApp_RestTemplate .
 
 
 
