454c39195980d8b4041d5083b5d59c07fbb35055
shankar7979 <shankar7979@gmail.com>
2024-01-09 10:05:30 +0530

    first commit

--- ss.bat
+++ ss.bat
@@ -1,12 +1,14 @@
 
   cp -r C:\Users\Training\eclipse-workspace1\SpringEurekaServer .
 cp -r C:\Users\Training\eclipse-workspace1\SpringRestJpaApp1 .
-
-
   cp -r C:\Users\Training\Downloads\SpringBootRestApp1\SpringBootRestApp1 .
 
-
 cp -r C:\Users\Training\eclipse-workspace1\SpringRestJpaApp2_Profiles .
+cp -r C:\Users\Training\eclipse-workspace1\SpringConfigClient .
+cp -r C:\Users\Training\eclipse-workspace1\SpringConfigServer .
+
+
+
 
  git add *
   git commit -m "first commit"
