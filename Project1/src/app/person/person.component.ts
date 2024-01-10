afa097bef38a887621f68eb62f410bc7131713fe
shankar7979 <shankar7979@gmail.com>
2024-01-10 14:52:57 +0530

    first commit

--- Project1/src/app/person/person.component.ts
+++ Project1/src/app/person/person.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Person } from './model/person';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-person',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './person.component.html',
   styleUrl: './person.component.css'
 })
 export class PersonComponent {
- person: Person;
+@Input() person: Person;
 
  constructor(){
    this.person=new Person();
