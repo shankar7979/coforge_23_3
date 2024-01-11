776d1bae6497f1d5c3c7c77f994e0830ba544a88
shankar7979 <shankar7979@gmail.com>
2024-01-11 10:52:20 +0530

    first commit

--- Project1/src/app/user/user.component.ts
+++ Project1/src/app/user/user.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { User } from './model/User';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+   user:User;
+   constructor(){
+    //this.user=new User(10001,'aman kumar',20000);
+    this.user=new User(0,'',0);
+   }
 }
