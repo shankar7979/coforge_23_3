4807e4ca85f4368996f4fdf9d65c52a64a68246e
shankar7979 <shankar7979@gmail.com>
2024-01-11 10:12:27 +0530

    first commit

--- Project1/src/app/pipe-example/pipe-example.component.ts
+++ Project1/src/app/pipe-example/pipe-example.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ExponentPipe } from "./exponent.pipe";
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-pipe-example',
     standalone: true,
     templateUrl: './pipe-example.component.html',
     styleUrl: './pipe-example.component.css',
-    imports: [ExponentPipe]
+    imports: [ExponentPipe,FormsModule]
 })
 export class PipeExampleComponent {
 
-   n1:number;
-   n2:number;
+ @Input()  n1:number;
+ @Input()  n2:number;
 
    constructor(){
      this.n1=3;
