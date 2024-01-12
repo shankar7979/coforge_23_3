ab82daedfaf5e135dd91abf9d921edc8071a5344
shankar7979 <shankar7979@gmail.com>
2024-01-12 12:24:06 +0530

    first commit

--- angular spring boot/Angular_crud/src/app/app.routes.ts
+++ angular spring boot/Angular_crud/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ShowallcustomerComponent } from './showallcustomer/showallcustomer.component';
+import { CustomeraddComponent } from './customeradd/customeradd.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'showall',component:ShowallcustomerComponent},
+    {path:'add',component:CustomeraddComponent},
+];
--- angular spring boot/Angular_crud/src/app/customeradd/customeradd.component.ts
+++ angular spring boot/Angular_crud/src/app/customeradd/customeradd.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from '@angular/core';
 import { CustomerService } from '../service/customer.service';
 import { Customer } from '../model/customer';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-customeradd',
@@ -14,13 +15,17 @@ import { FormsModule } from '@angular/forms';
 export class CustomeraddComponent {
   @Input() customer:Customer;
   msg:string='';
-constructor(private service:CustomerService){
+constructor(private service:CustomerService, private router:Router){
   this.customer=new Customer();
 }
 addcustomer(){
+  console.log('add customer called ')
   this.service.addCustomer(this.customer).subscribe(
-    data=>this.msg="record added",
+    data=>this.msg="record added"+JSON.stringify(data),
     error=>this.msg=error.error
   );
+}
+showall(){
+  this.router.navigate(['/showall']);
 }
 }
