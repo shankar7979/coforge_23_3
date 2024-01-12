751a6f68211ada8d6b9e1a0a9bc3b6e4c688debd
shankar7979 <shankar7979@gmail.com>
2024-01-12 15:08:34 +0530

    first commit

---
+++ react/reactapp1/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+          <>
+         <h1>hello world</h1>
+         <p>no1 is{x} </p>
+         <p>no2 is{y} </p>
+         <p>sum is{z} </p>
+        </>
+  );
+}
+let x=10; 
+let y=20; 
+let z=x+y; 
+
+export default App;
---
+++ react/reactapp1/src/App2.js
@@ -0,0 +1,16 @@
+export default function App2(){
+    return(
+        <div>
+          <h2>Reactjs Example</h2>
+          <h3>Employee Detail</h3>
+          <p>id is {emp.id}</p> 
+          <p>name is {emp.name}</p> 
+          <p>salary is {emp.salary}</p> 
+        </div>
+    )
+}
+let emp={
+    "id":10001,
+    "name":"suresh kumar",
+    "salary":20000,
+}
