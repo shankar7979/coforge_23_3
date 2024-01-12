010bb0299ba7156670231583dc1843cddd3c0b90
shankar7979 <shankar7979@gmail.com>
2024-01-12 15:25:20 +0530

    first commit

--- react/reactapp1/src/App2.js
+++ react/reactapp1/src/App2.js
@@ -1,11 +1,22 @@
+import './css/Hello.css';
+import emp1 from './image/emp1.jpg'
+import emp20 from './image/emp2.jpg'
 export default function App2(){
     return(
-        <div>
+        <div className='style1'>
           <h2>Reactjs Example</h2>
           <h3>Employee Detail</h3>
           <p>id is {emp.id}</p> 
           <p>name is {emp.name}</p> 
           <p>salary is {emp.salary}</p> 
+          <div>
+          <img src={emp1}></img>
+          </div>
+          
+          <div>
+          <img src={emp20}></img>
+          </div>
+
         </div>
     )
 }
--- ss.bat
+++ ss.bat
@@ -1,4 +1,4 @@
-cp -r C:\angular_project\Angular_crud\src  "angular spring boot\Angular_crud"
+rem cp -r C:\angular_project\Angular_crud\src  "angular spring boot\Angular_crud"
 
 rem  cp -r C:\Users\Training\eclipse-workspace1\SpringEurekaServer .
 rem cp -r C:\Users\Training\eclipse-workspace1\SpringRestJpaApp1 .
@@ -11,7 +11,7 @@ rem cp -r C:\Users\Training\eclipse-workspace1\SpringConfigServer .
 rem cp -r C:\Users\Training\eclipse-workspace1\SpringRestJpaApp_RestTemplate .
 
 
-
+cp -r C:\react_project\reactapp1\src react\reactapp1
 
  git add *
   git commit -m "first commit"
