fdc965de6c8855d1e3a6939988e967f9d1358d2a
shankar7979 <shankar7979@gmail.com>
2024-01-12 10:16:00 +0530

    first commit

--- angular spring boot/Angular_crud/src/app/service/customer.service.ts
+++ angular spring boot/Angular_crud/src/app/service/customer.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+
+  }
+
+  getAllCustomer():Observable<any>{
+   return  this.http.get("http://localhost:8080/customer/all")
+  }
+
 }
