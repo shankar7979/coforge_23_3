a7e3dee08744998673b4f01d5e40e5d064b6fe2e
shankar7979 <shankar7979@gmail.com>
2024-01-12 16:47:49 +0530

    first commit

--- react/reactapp1/src/Main.js
+++ react/reactapp1/src/Main.js
@@ -2,7 +2,7 @@ import { User } from "./User";
 
 const Main=()=>{
     return(
-        <User></User>
+        <User country='india' ></User>
         )
 }
 export default Main;
--- react/reactapp1/src/User.js
+++ react/reactapp1/src/User.js
@@ -1,7 +1,6 @@
 import { Component } from "react";
-
 export class User extends Component{
-    constructor(){
+    constructor(props){
       super();
       this.state={
         userid:10001,
@@ -16,6 +15,7 @@ export class User extends Component{
                 <p>Id is {this.state.userid}</p>
                 <p>Name is {this.state.username}</p>
                 <p>Salary is {this.state.usersalary}</p>
+                <p>Country is {this.props.country}</p>
             </div>
         )
     }
