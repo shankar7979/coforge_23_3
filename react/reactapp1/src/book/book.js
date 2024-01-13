190fce7a9ff88b9d8b245e6b06c74e9ae5ca2096
shankar7979 <shankar7979@gmail.com>
2024-01-13 10:55:44 +0530

    first commit

--- react/reactapp1/src/book/book.js
+++ react/reactapp1/src/book/book.js
@@ -12,24 +12,42 @@ export class Book extends Component {
     render() {
         return (
             <div>
-               <div className="bg-info text-primary p-5 m-5">
+               <div className="bg-warning text-primary p-5 m-5">
                  <h1>Book Component</h1>
-                  <table className="table table-striped">
+                  <table className="table table-striped table-bordered" 
+                      style={{'border':'solid blue'}}>
                     <tr>
                       <th>isbn</th>
                       <td>{this.state.isbn}</td>  
                     </tr>
                     
                     <tr>
-                      <th>isbn</th>
+                      <th>book name</th>
                       <td>{this.state.bname}</td>  
                     </tr>
                     <tr>
-                      <th>isbn</th>
+                      <th>cost</th>
                       <td>{this.state.cost}</td>  
                     </tr>
-
+                      <tr>
+                        <table className="table table-bordered" style={{'border':'dotted'}}>
+                            <tr className="bg-dark text-light">
+                                <th  colspan='3'>Publisher</th>
+                            </tr>
+                            <tr>
+                                <th>Id</th>
+                                <th>Name</th>
+                                <th>Location</th>
+                            </tr>
+                            <tr>
+                                <td>{this.props.book_publisher.id}</td>
+                                <td>{this.props.book_publisher.name}</td>
+                                <td>{this.props.book_publisher.location}</td>
+                            </tr>
+                        </table>
+                      </tr>
                   </table>
+
                  </div> 
             </div>
         )
