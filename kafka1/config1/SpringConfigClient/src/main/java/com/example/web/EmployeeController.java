17e0a5a3a6e3da8b8a4c4363101fc7377468f5bc
shankar7979 <shankar7979@gmail.com>
2024-01-13 16:46:17 +0530

    first commit

---
+++ kafka1/config1/SpringConfigClient/src/main/java/com/example/web/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.example.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Employee;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+	@Autowired
+	Employee employee;
+
+
+	@GetMapping("/emp1")
+	public Employee getEmployee() {
+		System.out.println("Employee is "+employee);
+		return employee;
+	}
+}
