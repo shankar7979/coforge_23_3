b038306455b466451ca04276c96859e521cf1076
shankar7979 <shankar7979@gmail.com>
2024-01-13 12:52:04 +0530

    first commit

--- react/reactapp1/src/student.js
+++ react/reactapp1/src/student.js
@@ -15,7 +15,7 @@ export class Student extends Component {
     render() {
         return (
             <>
-                <div className="container border border-primary bg-warning w-50 mt-5" style={{'borderRadius':'20px'}}>
+                <div className="container border border-primary bg-warning w-50 mt-5" style={{ 'borderRadius': '20px' }}>
                     <div style={{ 'border': 'inset 20px' }}>
                         <div>Roll is {this.state.roll}</div>
                         <div>Name is {this.state.name}</div>
@@ -23,19 +23,24 @@ export class Student extends Component {
                         <div>Marks is {this.state.marks}</div>
                         <div>Dob is {this.state.dob}</div>
                     </div>
-                    </div>
+                </div>
 
-                    <div className="container bg-info text-primary w-50 mt-5" style={{'borderRadius':'20px'}}>
-                        <hr></hr>
-                        <h2>School Address</h2>
-                        <div>Registation No {this.props.regno}</div>
-                        <div>Name {this.props.name}</div>
-                        <div>Location{this.props.location}</div>
-                        <div>
-                            Change Student Data
-                        </div>
-                    </div>
-                </>
-                )
+                <div className="container bg-info text-primary w-50 mt-5" style={{ 'borderRadius': '20px' }}>
+                    <hr></hr>
+                    <h2>School Address</h2>
+                    <div>Registation No {this.props.regno}</div>
+                    <div>Name {this.props.name}</div>
+                    <div>Location{this.props.location}</div>
+                </div>
+
+                <div className="container w-50 mt-5">
+
+                    <button className="btn btn-primary"> Change Student Data
+                    </button>
+
+                </div>
+
+            </>
+        )
     }
 }
