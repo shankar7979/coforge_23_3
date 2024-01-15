95454a64ccbf3ce3bec755015968e15fb7d20013
shankar7979 <shankar7979@gmail.com>
2024-01-15 16:50:13 +0530

    first commit

--- react/reactapp1/src/hook_form1.js
+++ react/reactapp1/src/hook_form1.js
@@ -1,22 +1,33 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export const Hook_form1 = () => {
     const [id, setId] = useState(100001);
     const [name, setName] = useState('ram kumar');
+    
+    let state='delhi';
+
+    useEffect=(()=>{
+        state='UP'
+        document.id=90001
+        document.name='shyam kumar'
+
+        console.log('use effect called ')
+    })
 
     return (
         <div>
             <h1>User Form</h1>
+             <p>State is {state}</p>
             <div className="form-group">
                 <label>Enter Id </label>
                 <input 
                 value={id}
-                className="form-control" onChange={() => setId(id)}></input>
+                className="form-control" onChange={(e) => setId(e.target.value)}></input>
             </div>
 
             <div className="form-group">
                 <label>Enter Name </label>
-                <input className="form-control" onChange={() => setName(name)}
+                <input className="form-control" onChange={(e) => setName(e.target.value)}
                 value={name}
                 ></input>
             </div>
