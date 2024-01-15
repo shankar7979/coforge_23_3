ce18ab91a9f15b4cca007f60f83f818fda2fbdfb
shankar7979 <shankar7979@gmail.com>
2024-01-15 16:38:57 +0530

    first commit

--- react/reactapp1/src/hook_form1.js
+++ react/reactapp1/src/hook_form1.js
@@ -1,22 +1,33 @@
 import { useState } from "react"
 
-export const Hook_form1=()=>{
-    const [id,setId]=useState(100001);
-    const [name,setName]=useState('ram kumar');
+export const Hook_form1 = () => {
+    const [id, setId] = useState(100001);
+    const [name, setName] = useState('ram kumar');
 
-    return(
+    return (
         <div>
             <h1>User Form</h1>
             <div className="form-group">
                 <label>Enter Id </label>
-                <input className="form-control" onChange={()=>setId(id)}></input>
+                <input 
+                value={id}
+                className="form-control" onChange={() => setId(id)}></input>
             </div>
-            
+
             <div className="form-group">
-                <label>Enter Id </label>
-                <input className="form-control" onChange={()=>setName(id)}></input>
+                <label>Enter Name </label>
+                <input className="form-control" onChange={() => setName(name)}
+                value={name}
+                ></input>
             </div>
-            <div className="">
+            <div className="border w-50 mt-5 bg-dark text-light p-5">
+                <div>
+                    id is {id}
+                </div>
+                
+                <div>
+                    name is {name}
+                </div>
 
             </div>
         </div>
