457f766c253714b91fb2b381b7ad437702fee721
shankar7979 <shankar7979@gmail.com>
2024-01-15 14:45:24 +0530

    first commit

--- react/reactapp1/src/RegistrationForm.js
+++ react/reactapp1/src/RegistrationForm.js
@@ -8,18 +8,32 @@ export default class RegistrationForm extends Component {
             name: ''
         };
     }
+    id_change=(e)=>{
+       if(e.target.value=='')
+       alert('id is blank')
+    }
+    
+    name_change=(e)=>{
+       if(e.target.value=='')
+       alert('name is blank')
+    }
+
     render() {
         return (
             <div>
                 <div className="container bg-info p-5  w-50" style={{ 'border-radius': '20px', 'marginLeft': '330px', 'marginTop': '50px' }}>
                     <div className="form-group">
                         <label>Enter Id</label>
-                        <input className="form-control" value={this.state.id}></input>
+                        <input className="form-control" 
+                         onChange={this.id_change}
+                        value={this.state.id}></input>
                     </div>
 
                     <div className="form-group">
                         <label>Enter Name</label>
-                        <input className="form-control" value={this.state.name}></input>
+                        <input className="form-control" value={this.state.name}
+                        onChange={this.name_change}
+                        ></input>
                     </div>
                     <div>
                         <button type="submit">Login</button>
