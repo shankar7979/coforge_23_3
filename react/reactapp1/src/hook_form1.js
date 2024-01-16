1485782742ce8621d13f900c87d8a7a011478c5c
shankar7979 <shankar7979@gmail.com>
2024-01-16 09:39:42 +0530

    first commit

--- react/reactapp1/src/hook_form1.js
+++ react/reactapp1/src/hook_form1.js
@@ -1,46 +1,54 @@
 import { useEffect, useState } from "react"
 
 export const Hook_form1 = () => {
-    const [id, setId] = useState(100001);
-    const [name, setName] = useState('ram kumar');
-    
-    let state='delhi';
+    const [id, setId] = useState(0);
+    const [name, setName] = useState('');
 
-    useEffect=(()=>{
-        state='UP'
-        document.id=90001
-        document.name='shyam kumar'
-
+    useEffect = (() => {
+        document.title='react hook form validation'
         console.log('use effect called ')
     })
 
     return (
         <div>
-            <h1>User Form</h1>
-             <p>State is {state}</p>
-            <div className="form-group">
-                <label>Enter Id </label>
-                <input 
-                value={id}
-                className="form-control" onChange={(e) => setId(e.target.value)}></input>
-            </div>
+            <div className="w-75 bg-info border p-5 m-5" style={{'border-radius':'20px'}}>
+                <h1>User Form</h1>
 
-            <div className="form-group">
-                <label>Enter Name </label>
-                <input className="form-control" onChange={(e) => setName(e.target.value)}
-                value={name}
-                ></input>
+                <div className="form-group">
+                    <label>Enter Id </label>
+                    <input
+                        value={id}
+                        className="form-control" onChange={(event) => 
+                        {if(event.target.value=='')
+                        alert('id is blank')
+                    else 
+                        setId(event.target.value)
+                        }
+                    }></input>
+                </div>
+
+                <div className="form-group">
+                    <label>Enter Name </label>
+                    <input className="form-control" onChange={(e) =>{
+                        if(e.target.value=='')
+                        alert('name is blank')
+                    else 
+                        setName(e.target.value)}}
+                        value={name}
+                    ></input>
+                </div>
             </div>
-            <div className="border w-50 mt-5 bg-dark text-light p-5">
+            <div className="w-75 bg-info border p-5 m-5" style={{'border-radius':'20px'}}>
                 <div>
                     id is {id}
                 </div>
-                
+
                 <div>
                     name is {name}
                 </div>
 
             </div>
+
         </div>
     )
 }
