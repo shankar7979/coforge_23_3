46d041f9ea8479192eb04e42dffa37bf78d3fc85
shankar7979 <shankar7979@gmail.com>
2024-01-31 16:44:24 +0530

    first commit

--- kafka1/spring_security/App1/src/main/java/org/abc/Main.java
+++ kafka1/spring_security/App1/src/main/java/org/abc/Main.java
@@ -1,12 +1,11 @@
-package abc.com;
+package org.abc;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
-
-  public static void main(String[] args) {
-    SpringApplication.run(App.class, args);
-  }
-
+public class Main{
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
