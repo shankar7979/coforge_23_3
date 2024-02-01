41ad5f2fd12413cfc1c3426c78049fe8f0298bf4
shankar7979 <shankar7979@gmail.com>
2024-02-01 13:10:56 +0530

    first commit

--- kafka1/spring_security/spring-boot2/App1 basic authentication/src/main/java/org/abc/config/ProjectConfig.java
+++ kafka1/spring_security/spring-boot2/App1 basic authentication/src/main/java/org/abc/config/ProjectConfig.java
@@ -1,34 +1,36 @@
 package org.abc.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-public class ProjectConfig  extends WebSecurityConfigurerAdapter {
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     @Override
+
     public UserDetailsService userDetailsServiceBean() throws Exception {
-        var userDetailService=new InMemoryUserDetailsManager();
-        var user= User.withUsername("ram kumar").password("abcd").authorities("read").build();
+        var userDetailService = new InMemoryUserDetailsManager();
+        var user = User.withUsername("ram kumar").password("abcd").authorities("read").build();
         userDetailService.createUser(user);
-        return  userDetailService;
+        return userDetailService;
     }
 
     @Bean
-    public PasswordEncoder getPassordEncoder(){
+    public PasswordEncoder getPassordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    http.httpBasic();
-    http.authorizeRequests().anyRequest().authenticated();
+        http.httpBasic();
+        http.authorizeRequests().anyRequest().authenticated();
     }
 }
--- kafka1/spring_security/spring_boot3/form_authetication/App2/src/main/java/org/abc/controller/UserController.java
+++ kafka1/spring_security/spring_boot3/form_authetication/App2/src/main/java/org/abc/controller/UserController.java
@@ -1,15 +1,13 @@
-package org.abc.controller
+package org.abc.controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMappping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-@RequestMappping("/")
-public class HelloController{
- 
+public class UserController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello!";
+        return "Hello  World!";
+
     }
 }
